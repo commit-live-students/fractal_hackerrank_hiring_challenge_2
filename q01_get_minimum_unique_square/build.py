5fedfa28c960b3dcafc620b1be0acca849b29561
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-05-17 12:02:36 +0000

    Done

--- q01_get_minimum_unique_square/build.py
+++ q01_get_minimum_unique_square/build.py
@@ -5,5 +5,9 @@ from math import sqrt
 # Write your solution here:
 
 def q01_get_minimum_unique_square(x,y):
-    "write your solution here"
-    
+    count = 0
+    for i in range(1,x+1):
+        if i**2 >= x and i**2 <= y:
+            count = count + 1
+
+    return count
