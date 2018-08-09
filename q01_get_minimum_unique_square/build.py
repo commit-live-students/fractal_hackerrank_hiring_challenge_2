069d285853c586e79a9a17dd1c0e5efee3cef652
ankit-a-mehta <ankitmehta195@gmail.com>
2018-08-09 09:57:04 +0000

    Done

--- q01_get_minimum_unique_square/build.py
+++ q01_get_minimum_unique_square/build.py
@@ -1,3 +1,4 @@
+# %load q01_get_minimum_unique_square/build.py
 # Default imports
 
 from math import sqrt
@@ -5,5 +6,14 @@ from math import sqrt
 # Write your solution here:
 
 def q01_get_minimum_unique_square(x,y):
-    "write your solution here"
-    
+    square_array = []
+    for num in range(2,int(y+1/2)):
+        num_square = num**2
+        if(num_square >= int(x) and num_square <= int(y)):
+            square_array.append(num)
+        if(num_square > y):
+            break
+    return len(square_array)
+
+q01_get_minimum_unique_square(4,16)
+
