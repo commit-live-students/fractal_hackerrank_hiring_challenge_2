8ac12ce5c70dab1381a7db36e5823a3c8f12634b
ramyakrishnan86 <ramya_krishnan86@yahoo.co.in>
2018-08-22 13:48:51 +0000

    Done

--- q01_get_minimum_unique_square/build.py
+++ q01_get_minimum_unique_square/build.py
@@ -1,3 +1,4 @@
+# %load q01_get_minimum_unique_square/build.py
 # Default imports
 
 from math import sqrt
@@ -5,5 +6,18 @@ from math import sqrt
 # Write your solution here:
 
 def q01_get_minimum_unique_square(x,y):
-    "write your solution here"
-    
+    'write your solution here'
+    prfctsqrt = []
+    List = range(x, y+1)
+    prfctsqrt = [num for num in List if int(sqrt(num)) * int(sqrt(num)) == num]
+#    for num in range(x, y+1):
+#        if (num == 1):
+#            continue
+#        else:
+#            sqrtnum = int(sqrt(num))
+#            if (sqrtnum * sqrtnum == num):
+#                prfctsqrt.append(num)
+    return len(prfctsqrt)
+q01_get_minimum_unique_square(4, 25)
+
+
