461404483cb1c026c30a5cebb3fc786f58a0af95
dhananjay93 <dhananjay.hawal@gmail.com>
2018-09-29 05:43:16 +0000

    Done

--- q01_get_minimum_unique_square/build.py
+++ q01_get_minimum_unique_square/build.py
@@ -1,9 +1,15 @@
+# %load q01_get_minimum_unique_square/build.py
 # Default imports
-
+import numpy as np
 from math import sqrt
 
 # Write your solution here:
 
 def q01_get_minimum_unique_square(x,y):
-    "write your solution here"
+    'write your solution here'
+    li = list(np.arange(x,y+1))
+    final_list = list(filter(lambda x: ((x**0.5)%2 == 0 or (x**0.5)%2 == 1) , li))    
+    return len(final_list)
     
+
+
