86fcac791b970886eaa4ed88fc1c964df9264268
Lavi1920 <lavitha1920@gmail.com>
2018-09-29 05:23:38 +0000

    Done

--- q01_get_minimum_unique_square/build.py
+++ q01_get_minimum_unique_square/build.py
@@ -1,9 +1,15 @@
+# %load q01_get_minimum_unique_square/build.py
 # Default imports
-
+import math
 from math import sqrt
 
 # Write your solution here:
 
-def q01_get_minimum_unique_square(x,y):
-    "write your solution here"
-    
+def q01_get_minimum_unique_square(a,b):
+    'write your solution here' 
+    return (math.floor(math.sqrt(b)) - math.ceil(math.sqrt(a)) + 1) 
+
+q01_get_minimum_unique_square(3,9)
+
+
+
