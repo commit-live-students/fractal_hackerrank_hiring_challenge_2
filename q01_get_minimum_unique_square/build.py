aeb546c2a0f7e28a84fd58fc04b059839b8193fa
preetiail <ailpreeti@ymail.com>
2018-09-29 06:43:00 +0000

    Done

--- q01_get_minimum_unique_square/build.py
+++ q01_get_minimum_unique_square/build.py
@@ -1,9 +1,16 @@
+# %load q01_get_minimum_unique_square/build.py
 # Default imports
-
+import numpy as np
 from math import sqrt
 
 # Write your solution here:
-
 def q01_get_minimum_unique_square(x,y):
-    "write your solution here"
+    output=0
+    list1=np.arange(x,y+1)
+    for num in list1:
+        output= output+1 if np.sqrt(num).is_integer() else output+0
+    return(output)
     
+q01_get_minimum_unique_square(3,9)
+
+
