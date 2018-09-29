c705ae8c28f1e2c5d896fb039f0471aa96b4a6fe
vivekshingate <vivekshingate@ymail.com>
2018-09-29 05:57:36 +0000

    Done

--- q01_get_minimum_unique_square/build.py
+++ q01_get_minimum_unique_square/build.py
@@ -1,3 +1,4 @@
+# %load q01_get_minimum_unique_square/build.py
 # Default imports
 
 from math import sqrt
@@ -5,5 +6,12 @@ from math import sqrt
 # Write your solution here:
 
 def q01_get_minimum_unique_square(x,y):
-    "write your solution here"
+    Square_list = [items for items in range(x,y+1) if items**0.5==int(items**0.5)]
+    return len(Square_list)
+
+q01_get_minimum_unique_square(3,9)
     
+
+
+
+
